ae2b87e33e5d6f562e932e86f2197da19d5e9777
Matteo Maggiolini <webinadvance@gmail.com>
2024-11-20 15:14:15 +0100

    Auto commit: 2024-11-20 15:14:14

--- client/src/hooks.server.js
+++ client/src/hooks.server.js
@@ -1,18 +1,4 @@
 ﻿/** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-  const response = await fetch(
-    "http://localhost:3000/api/pages?publishedOnly=true",
-  );
-  const pages = await response.json();
-
-  // Find matching redirect
-  const redirect = pages.find(
-    (page) => page.redirectUrl && event.url.pathname === "/" + page.slug,
-  );
-
-  if (redirect) {
-    return Response.redirect(new URL(redirect.redirectUrl, event.url.origin));
-  }
-
   return resolve(event);
 }
