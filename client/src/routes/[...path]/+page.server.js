828e7c622a8eff426299c206610f5e17a61ef114
Matteo Maggiolini <webinadvance@gmail.com>
2024-11-20 21:14:30 +0100

    Auto commit: 2024-11-20 21:14:29

--- client/src/routes/[...path]/+page.server.js
+++ client/src/routes/[...path]/+page.server.js
@@ -3,24 +3,23 @@ config()
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ fetch, params }) {
+  const { VITE_API_BASE_URL } = process.env
   const pathParts = params.path.split('/')
 
   for (let i = pathParts.length; i > 0; i--) {
     const testPath = pathParts.slice(0, i).join('/')
-    const apiBaseUrl = process.env.VITE_API_BASE_URL
-    const response = await fetch(`${apiBaseUrl}/api/pages/${testPath}`)
-
+    const response = await fetch(`${VITE_API_BASE_URL}/api/pages/${testPath}`)
     if (response.ok) {
       const page = await response.json()
-      const paramValues = pathParts.slice(i)
-
-      const routeParams = {}
-      ;(page.paramSchema || []).forEach((name, index) => {
-        routeParams[name] = paramValues[index]
-      })
-
+      const routeParams = Object.fromEntries(
+        (page.paramSchema || []).map((name, index) => [
+          name,
+          pathParts.slice(i)[index],
+        ]),
+      )
       return { page, routeParams }
     }
   }
+
   return { page: null }
 }
