d283730a0578113b37780671a7461f9a7bbd01f0
Matteo Maggiolini <webinadvance@gmail.com>
2024-12-01 20:58:54 +0100

    Use callback in app.listen for server start log
    Added a callback function to app.listen to log a message when the server starts. This change improves visibility into the server's running state by providing clear confirmation and connection details in the console output.

--- app/server.js
+++ app/server.js
@@ -13,4 +13,8 @@ app.use(errorHandler)
 app.use(await createViteMiddleware())
 await initializeDatabase()
 await addSampleData()
-app.listen(process.env.PORT)
+
+const port = process.env.PORT
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`),
+)
