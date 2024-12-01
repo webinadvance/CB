4c9346892d040d998e18e0f4bd5fc0afd4f75fd6
Matteo Maggiolini <webinadvance@gmail.com>
2024-12-01 20:57:55 +0100

    Refactor server initialization for clarity
    Simplify server initialization by removing the createDevServer function. Directly configure middleware, initialize the database, and start the server in a more streamlined manner. This change enhances readability and reduces unnecessary nesting.

--- app/server.js
+++ app/server.js
@@ -1,43 +1,16 @@
-﻿import express from 'express'
+﻿import 'dotenv/config'
+import express from 'express'
 import initializeDatabase from './database/index.js'
 import addSampleData from './database/sampleData.js'
 import createViteMiddleware from './config/viteConfig.js'
 import pagesRouter from './api/pages.js'
 import errorHandler from './middleware/errorHandler.js'
 
-async function createDevServer() {
-  const app = express()
-  const port = process.env.PORT || 3000
-
-  app.use(express.json())
-
-  // API routes
-  app.use('/api/pages', pagesRouter)
-
-  app.use(errorHandler)
-
-  // Vite middleware
-  try {
-    const viteMiddleware = await createViteMiddleware()
-    app.use(viteMiddleware)
-  } catch (error) {
-    console.error('Unable to setup Vite middleware:', error)
-    process.exit(1)
-  }
-
-  // Database initialization and sample data
-  try {
-    await initializeDatabase()
-    await addSampleData()
-  } catch (error) {
-    console.error('Failed to initialize database or add sample data:', error)
-    process.exit(1)
-  }
-
-  // Start the server
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
-  })
-}
-
-createDevServer()
+const app = express()
+app.use(express.json())
+app.use('/api/pages', pagesRouter)
+app.use(errorHandler)
+app.use(await createViteMiddleware())
+await initializeDatabase()
+await addSampleData()
+app.listen(process.env.PORT)
