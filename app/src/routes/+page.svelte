4f01d5c791742fe1dbf9864e1a509b7ca8dc3342
Matteo Maggiolini <webinadvance@gmail.com>
2024-12-03 21:26:47 +0100

    Improve formatting and update placeholder text
    Refactor HTML tags for better readability by adjusting line breaks. Modify placeholder text from "dasssdsssssaasassssssss" to "NOWWWWWWWWWW" for clarity.

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
 
 <nav>
-    <ul>
-        <li><a href="/test">Go to Test Page</a></li>
-        <div>dasssdsssssaasassssssss</div>
-    </ul>
+  <ul>
+    <li><a href="/test">Go to Test Page</a></li>
+    <div>NOWWWWWWWWWW</div>
+  </ul>
 </nav>
