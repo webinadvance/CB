505b5c0ca150df57e0c602b2f98d1f4f67c5249a
Matteo Maggiolini <webinadvance@gmail.com>
2024-12-07 23:39:56 +0100

    feat(TestComponent2): display params in UI

--- app/src/lib/components/TestComponent2.svelte
+++ app/src/lib/components/TestComponent2.svelte
@@ -2,8 +2,10 @@
   import { BaseComponent, getMediaPreviewUrl } from '$lib'
   let src = getMediaPreviewUrl(1)
 </script>
+
 <BaseComponent {...$$props} let:pageData let:params let:content>
   <div class="p-4">
+    <div>{JSON.stringify(params)}</div>
     <div>{content('main-content')}</div>
   </div>
 </BaseComponent>
