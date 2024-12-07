4c27db568f06d6367bc1649d0c909fe5090f7585
Matteo Maggiolini <webinadvance@gmail.com>
2024-12-07 22:51:31 +0100

    fix(i18n): sync language via cookies, remove HttpOnly attr

--- app/src/hooks.server.js
+++ app/src/hooks.server.js
@@ -1,14 +1,15 @@
 import { i18n } from '$lib/i18n'
 
 export async function handle({ event, resolve }) {
-  const lang = event.request.headers.get('cookie')?.match(/language=(\w+)/)?.[1] || 'en'
+  const lang =
+    event.request.headers.get('cookie')?.match(/language=(\w+)/)?.[1] || 'en'
   i18n.language = lang
-  
+
   const response = await resolve(event)
-  
+
   if (!event.request.headers.get('cookie')?.includes('language=')) {
-    response.headers.append('Set-Cookie', `language=${lang}; Path=/; HttpOnly`)
+    response.headers.append('Set-Cookie', `language=${lang}; Path=/`)
   }
-  
+
   return response
 }
--- app/src/lib/i18n.js
+++ app/src/lib/i18n.js
@@ -1,11 +1,15 @@
 export const i18n = {
-  language: typeof localStorage !== 'undefined' ? localStorage.getItem('language') || 'en' : 'en',
+  language:
+    typeof localStorage !== 'undefined'
+      ? localStorage.getItem('language') || 'en'
+      : 'en',
   content: {},
   changeLanguage(lang) {
     this.language = lang
     if (typeof localStorage !== 'undefined') {
       localStorage.setItem('language', lang)
+      document.cookie = `language=${lang}; Path=/;`
       window.location.reload()
     }
-  }
+  },
 }
