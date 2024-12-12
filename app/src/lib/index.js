3da84eb9fe2e33f4d3c021c27887c36a69c125c2
Matteo Maggiolini <webinadvance@gmail.com>
2024-12-12 19:51:17 +0100

    `refactor: remove unused media exports`

--- app/src/lib/index.js
+++ app/src/lib/index.js
@@ -1,3 +1,2 @@
 export { default as TestComponent2 } from './components/TestComponent2.svelte'
 export { default as HomeComponent } from './components/HomeComponent.svelte'
-export * from './utils/media.js'
