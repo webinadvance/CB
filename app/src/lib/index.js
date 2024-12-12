f25fdcab86440402eb5389f483a5b61ddc4ef287
Matteo Maggiolini <webinadvance@gmail.com>
2024-12-12 19:50:23 +0100

    `chore: remove unused svelte context actions`

--- app/src/lib/index.js
+++ app/src/lib/index.js
@@ -1,5 +1,3 @@
-import { getContent } from '$lib/actions/content.js'
-export { getContent }
 export { default as TestComponent2 } from './components/TestComponent2.svelte'
 export { default as HomeComponent } from './components/HomeComponent.svelte'
 export * from './utils/media.js'
