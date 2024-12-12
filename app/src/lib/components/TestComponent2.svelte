03206c466ced19b8772d995a80ef9049626c98d5
Matteo Maggiolini <webinadvance@gmail.com>
2024-12-12 20:21:33 +0100

    refactor: remove unused imports and commented code in TestComponent2

--- app/src/lib/components/TestComponent2.svelte
+++ app/src/lib/components/TestComponent2.svelte
@@ -1,8 +1,5 @@
 <script>
   import { getPageData } from '$lib/stores/pageStore'
-  import EditableContent from '$lib/components/EditableContent.svelte'
-  const pageData = getPageData()
 </script>
 
 <div>TestComponent2.svelte</div>
-<!--<div>{JSON.stringify(pageData)}</div>-->
