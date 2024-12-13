e8f55a0249e87f89106514b29c81ee24be5e1a10
Matteo Maggiolini <webinadvance@gmail.com>
2024-12-13 23:39:20 +0100

    fix: enable session loading in layout server

--- app/src/routes/[lang]/+layout.server.js
+++ app/src/routes/[lang]/+layout.server.js
@@ -1,6 +1,6 @@
 ﻿export async function load({ locals, params }) {
   return {
     lang: locals.lang || params.lang || 'en',
-    // session: await locals.auth(),
+    session: await locals.auth(),
   }
 }
