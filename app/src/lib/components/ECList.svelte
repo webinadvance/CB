352254ff52fa9296c115dd1002e6d2482a4999c9
Matteo Maggiolini <webinadvance@gmail.com>
2024-12-15 22:31:28 +0100

    refactor: clean ECList logic, remove unused code/comments

--- app/src/lib/components/ECList.svelte
+++ app/src/lib/components/ECList.svelte
@@ -1,22 +1,18 @@
 ﻿<script>
   import { pageData } from '$lib/stores/pageStore'
   import { isEditable } from '$lib/stores/editorStore'
-  import { Plus } from 'lucide-svelte'
 
   export let key
   let items = []
 
-  // Recalculate items based on $pageData updates
   $: {
     const existingItems = Object.entries($pageData.contentData || {})
       .filter(([k, v]) => k.startsWith(`${key}.`) && v)
       .map(([k]) => Number(k.split('.')[1]))
       .sort((a, b) => a - b)
 
-    // Reindex to fill gaps
     items = existingItems.map((_, idx) => idx)
 
-    // Update store with reindexed items if needed
     existingItems.forEach((oldIndex, newIndex) => {
       if (oldIndex !== newIndex) {
         const oldKey = `${key}.${oldIndex}`
@@ -36,7 +32,6 @@
   }
 
   function addNewItem() {
-    // Add a new item at the next available index
     items = [...items, items.length]
   }
 </script>
@@ -52,16 +47,13 @@
   <div class="relative {$$props.class}">
     {#each items as item, index}
       <slot itemKey={`${key}.${index}`} timestamp={Date.now()} />
-      {#if index === items.length - 1}
-        <!--        <button-->
-        <!--          class="absolute top-0 left-0 opacity-80 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded flex-shrink-0"-->
-        <!--          on:click={addNewItem}>Add New</button-->
-        <!--        >-->
-      {/if}
+      {#if index === items.length - 1}{/if}
     {/each}
     <button
       class="absolute top-0 left-0 opacity-80 opacity-80 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded flex-shrink-0"
-      on:click={addNewItem}>Add New</button
+      on:click={addNewItem}
     >
+      Add New
+    </button>
   </div>
 {/if}
